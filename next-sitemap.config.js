ce1d9d7fd48478995ea6fc82f8f510f092461fd1
himanshu pal <himanshupal2412@gmail.com>
2024-04-17 19:36:41 +0530

    initial commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+const siteMetadata = require("./src/utils/siteMetaData");
+
+
+module.exports ={
+    siteUrl: siteMetadata.siteUrl,
+    generateRobotsTxt: true,
+   
+}
---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,17 @@
+
+const siteMetadata = {
+    title: 'Blog Site',
+    author: 'TechnoBlog',
+    headerTitle: 'Blog Site',
+    description: 'Technical Blogs',
+    language: 'en-us',
+    theme: 'system',
+    siteUrl: 'https://create-blog-with-nextjs.vercel.app', 
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', 
+    email: 'himanshupal2412@gmail.com', 
+    linkedin: 'https://www.linkedin.com/in/himanshupal24/',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
