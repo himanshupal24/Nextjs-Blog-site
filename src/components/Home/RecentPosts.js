bbdae429b5ef8f57859c41ca4ff24c88b820c817
Himanshu Pal <121677669+himanshupal24@users.noreply.github.com>
2024-05-08 20:03:53 +0530

    Update RecentPosts.js

--- src/components/Home/RecentPosts.js
+++ src/components/Home/RecentPosts.js
@@ -6,6 +6,7 @@ import BlogLayoutThree from "../Blog/BlogLayoutThree";
 const RecentPosts = ({ blogs }) => {
   const sortedBlogs = sortBlogs(blogs);
   return (
+    <meta name="google-adsense-account" content="ca-pub-7070336929107463">
     <section className="w-full  mt-16 sm:mt-24  md:mt-32 px-5 sm:px-10 md:px-24  sxl:px-32 flex flex-col items-center justify-center">
       <div className="w-full flex  justify-between">
         <h2 className="w-fit inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">
