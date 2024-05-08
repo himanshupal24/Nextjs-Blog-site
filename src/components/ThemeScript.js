f1ce66d86b70109103ece8517e0d6f956b8849e7
Himanshu Pal <121677669+himanshupal24@users.noreply.github.com>
2024-05-08 20:04:31 +0530

    Update ThemeScript.js

--- src/components/ThemeScript.js
+++ src/components/ThemeScript.js
@@ -7,6 +7,7 @@ import Script from 'next/script';
 
 const ThemeScript = () => {
     return (
+        <meta name="google-adsense-account" content="ca-pub-7070336929107463">
         <Script id="theme-switcher" strategy="beforeInteractive">
             {`if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
