76d11886db5445cd482a64c6a5d2675cbebd7481
Himanshu Pal <121677669+himanshupal24@users.noreply.github.com>
2024-05-08 19:59:01 +0530

    Update BlogLayoutThree.js

--- src/components/Blog/BlogLayoutThree.js
+++ src/components/Blog/BlogLayoutThree.js
@@ -5,6 +5,7 @@ import React from "react";
 
 const BlogLayoutThree = ({ blog }) => {
   return (
+    <meta name="google-adsense-account" content="ca-pub-7070336929107463">
     <div className="group flex flex-col items-center text-dark dark:text-light">
       <Link href={blog.url} className="h-full rounded-xl overflow-hidden">
         <Image
