c873da98c0c9e68880138e39e2b770f9b602f3a6
Himanshu Pal <121677669+himanshupal24@users.noreply.github.com>
2024-05-08 19:59:38 +0530

    Update Categories.js

--- src/components/Blog/Categories.js
+++ src/components/Blog/Categories.js
@@ -4,6 +4,7 @@ import Category from "./Category";
 
 const Categories = ({ categories, currentSlug }) => {
   return (
+    <meta name="google-adsense-account" content="ca-pub-7070336929107463">
     <div className=" px-0 md:px-10 sxl:px-20 mt-10 border-t-2 text-dark dark:text-light border-b-2 border-solid border-dark dark:border-light py-4 flex items-start flex-wrap font-medium mx-5 md:mx-10">
       {categories.map((cat) => (
         <Category
