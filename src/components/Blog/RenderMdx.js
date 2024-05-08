c42b85e1db7f0cf7121dbdda945efea1f53b84e2
Himanshu Pal <121677669+himanshupal24@users.noreply.github.com>
2024-05-08 20:00:22 +0530

    Update RenderMdx.js

--- src/components/Blog/RenderMdx.js
+++ src/components/Blog/RenderMdx.js
@@ -13,6 +13,7 @@ const RenderMdx = ({blog}) => {
     const MDXContent = useMDXComponent(blog.body.code)
 
   return (
+    <meta name="google-adsense-account" content="ca-pub-7070336929107463">
     <div className='col-span-12  lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
     prose-blockquote:bg-accent/20 
     prose-blockquote:p-2
